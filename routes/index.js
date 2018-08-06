e6d1f152d7ebcbae7c12e44030e647cfcefc8546
Ricky Fok <kayufok91@gmail.com>
2018-08-07 02:08:42 +0800

    pug header and footer

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('newIndex', { title: 'Express' });
+  res.render('conceptIndex', { title: 'Express' });
 });
+
+router.get('/index', function(req, res, next) {
+  res.render('conceptIndex', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('conceptAbout', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('conceptContact', { title: 'Express' });
+});
+
+router.get('/products', function(req, res, next) {
+  res.render('conceptProduct', { title: 'Express' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('conceptServices', { title: 'Express' });
+});
+
 
 module.exports = router;
